863a4a6536f2bfaae6c1f814a052f419c0aeb5ac
Vinícius dos Santos Mello <vinicius.mello@intuitivecare.com>
2020-01-14 19:21:42 -0300

    Started the development of an VAE

---
+++ Packages/NoveltyDetection/VariationalAutoencoders/models/vae.py
@@ -0,0 +1,61 @@
+from tensorflow.keras import Model, Input, Dense, Sequential
+
+class VariationalAutoencoder:
+    def __init__(self):
+        tf.
+
+    # VAE model = encoder + decoder
+    def create_encoder(self, latent_dim):
+        # build encoder model
+        inputs = Input(shape=input_shape, name='encoder_input')
+        x = Dense(intermediate_dim, activation='relu')(inputs)
+        z_mean = Dense(latent_dim, name='z_mean')(x)
+        z_log_var = Dense(latent_dim, name='z_log_var')(x)
+
+    def create_decoder(self, latent_dim):
+        pass
+
+
+def main(**kwargs):
+    # VAE model = encoder + decoder
+    # build encoder model
+    inputs = Input(shape=input_shape, name='encoder_input')
+    x = Dense(intermediate_dim, activation='relu')(inputs)
+    z_mean = Dense(latent_dim, name='z_mean')(x)
+    z_log_var = Dense(latent_dim, name='z_log_var')(x)
+
+    # use reparameterization trick to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+
+    # instantiate encoder model
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
+    plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
+
+    # build decoder model
+    latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+    x = Dense(intermediate_dim, activation='relu')(latent_inputs)
+    outputs = Dense(original_dim, activation='sigmoid')(x)
+
+    # instantiate decoder model
+    decoder = Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
+    plot_model(decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
+
+    # instantiate VAE model
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae_mlp')
+
+
+if __name__ == 'main':
+    encoder = create_encoder()
+    decoder = create_decoder()
+    sampler = create_sampler()
+
+    x = layers.Input(shape=image_shape)
+    t_mean, t_log_var = encoder(x)
+    t = sampler([t_mean, t_log_var])
+    t_decoded = decoder(t)
+
+    vae = Model(x, t_decoded, name='vae')
