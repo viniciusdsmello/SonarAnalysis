fcd5f958ae55cc2a578cb5ac55d9a8f6b17c48ba
Vinícius dos Santos Mello <vinicius.mello@intuitivecare.com>
2020-01-19 16:48:51 -0300

    Created VariationalAutoencoder class

--- Packages/NoveltyDetection/VariationalAutoencoders/models/vae.py
+++ Packages/NoveltyDetection/VariationalAutoencoders/models/vae.py
@@ -1,62 +1,91 @@
-from tensorflow.keras import Model, Input, Dense, Sequential
+import tensorflow as tf
+import numpy as np
 
 
-class VariationalAutoencoder:
-    def __init__(self):
-        pass
+class VariationalAutoencoder(tf.keras.Model):
+    def __init__(self, original_dim, intermediate_dim, latent_dim, name='variational_autoencoder', **kwargs):
+        super(VariationalAutoencoder, self).__init__(name=name, **kwargs)
 
-    # VAE model = encoder + decoder
-    def create_encoder(self, latent_dim):
+        self._original_dim = original_dim
+        self._intermediate_dim = intermediate_dim
+        self._latent_dim = latent_dim
+
+        self.encoder = Encoder(self._intermediate_dim, self._latent_dim)
+        self.decoder = Decoder(self._intermediate_dim, self._original_dim)
+
+    def call(self, inputs):
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstructed = self.decoder(z)
+        kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+        kl_loss = -0.5 * tf.reduce_sum(kl_loss)
+        self.add_loss(kl_loss)
+        return reconstructed
+
+
+class Sampling(tf.keras.layers.Layer):
+    # reparameterization trick
+    # instead of sampling from Q(z|X), sample eps = N(0,I)
+    # z = z_mean + sqrt(var)*eps
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, intermediate_dim, latent_dim, name='encoder', **kwargs):
+        super(Encoder, self).__init__(name=name, **kwargs)
         # build encoder model
-        inputs = Input(shape=input_shape, name='encoder_input')
-        x = Dense(intermediate_dim, activation='relu')(inputs)
-        z_mean = Dense(latent_dim, name='z_mean')(x)
-        z_log_var = Dense(latent_dim, name='z_log_var')(x)
+        self.x = tf.keras.layers.Dense(intermediate_dim, activation=tf.nn.relu, name='encoder_intermediate')
+        self.z_mean = tf.keras.layers.Dense(latent_dim, name='z_mean')
+        self.z_log_var = tf.keras.layers.Dense(latent_dim, name='z_log_var')
+        self.sampling = Sampling()
 
-    def create_decoder(self, latent_dim):
-        pass
+    def call(self, inputs):
+        y = self.x(inputs)
+        z_mean = self.z_mean(y)
+        z_log_var = self.z_log_var(y)
+        z = self.sampling((z_mean, z_log_var))
+        return z_mean, z_log_var, z
+
+
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self, intermediate_dim, original_dim, name='decoder', **kwargs):
+        super(Decoder, self).__init__(name=name, **kwargs)
+        # build decoder model
+        self.x = tf.keras.layers.Dense(intermediate_dim, activation=tf.nn.relu, name='decoder_intermediate')
+        self.outputs = tf.keras.layers.Dense(original_dim, activation=tf.nn.sigmoid, name='decoder_output')
+
+    def call(self, latent_inputs):
+        y = self.x(latent_inputs)
+        return self.outputs(y)
 
 
 def main(**kwargs):
-    # VAE model = encoder + decoder
-    # build encoder model
-    inputs = Input(shape=input_shape, name='encoder_input')
-    x = Dense(intermediate_dim, activation='relu')(inputs)
-    z_mean = Dense(latent_dim, name='z_mean')(x)
-    z_log_var = Dense(latent_dim, name='z_log_var')(x)
+    # MNIST dataset
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    # use reparameterization trick to push the sampling out as input
-    # note that "output_shape" isn't necessary with the TensorFlow backend
-    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+    image_size = x_train.shape[1]
+    original_dim = image_size * image_size
+    x_train = np.reshape(x_train, [-1, original_dim])
+    x_test = np.reshape(x_test, [-1, original_dim])
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
 
-    # instantiate encoder model
-    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-    encoder.summary()
-    plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
+    # network parameters
+    input_shape = (original_dim,)
+    intermediate_dim = 512
+    batch_size = 128
+    latent_dim = 2
+    epochs = 50
 
-    # build decoder model
-    latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-    x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-    outputs = Dense(original_dim, activation='sigmoid')(x)
-
-    # instantiate decoder model
-    decoder = Model(latent_inputs, outputs, name='decoder')
-    decoder.summary()
-    plot_model(decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
-
-    # instantiate VAE model
-    outputs = decoder(encoder(inputs)[2])
-    vae = Model(inputs, outputs, name='vae_mlp')
+    model = VariationalAutoencoder(original_dim, intermediate_dim, latent_dim)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    model.compile(optimizer, loss=tf.keras.losses.mse)
+    model.fit(x_train, x_train, epochs=3, batch_size=64)
 
 
 if __name__ == 'main':
-    encoder = create_encoder()
-    decoder = create_decoder()
-    sampler = create_sampler()
-
-    x = layers.Input(shape=image_shape)
-    t_mean, t_log_var = encoder(x)
-    t = sampler([t_mean, t_log_var])
-    t_decoded = decoder(t)
-
-    vae = Model(x, t_decoded, name='vae')
+    main()
